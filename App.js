630b53a48ab619c5f31b29829fd0f21567475ca1
Ganesh Yadla <mizeganesh@gmail.com>
2022-12-25 10:02:33 +0530

    updated app file

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-const heading1 = React.createElement("h1", {
+const heading1 = React.createElement(
+  "h1",
+  {
     id: "heading-one",
-    class: "heading-cl1"
-}, "Hi Everybody, I am heading one H1");
-const heading2 = React.createElement("h1", {
+    class: "heading-cl1",
+  },
+  "Hi Everybody, I am heading one H1"
+);
+const heading2 = React.createElement(
+  "h1",
+  {
     id: "heading-two",
-    class: "heading-cl2"
-}, "Hi Everybody, I am heading one H2");
+    class: "heading-cl2",
+  },
+  "Hi Everybody, I am heading one H2"
+);
+const container = React.createElement(
+  "h1",
+  {
+    id: "container",
+  },
+  [heading1, heading2]
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render([heading1, heading2]);
+root.render(container);
